737cbc8f15c14dd9da397dbcfcec5cc6327f51cd
Виктория Князькова <vknyazkova01@gmail.com>
2023-10-18 04:33:06 +0300

    some updates

--- thesis_search/webui/app.py
+++ thesis_search/webui/app.py
@@ -8,9 +8,6 @@ from thesis_search.search import search_theses
 app = Flask(__name__)
 db, fast_nlp, corpus, default_params, search_engines = init_defaults()
 
-initialized_search_engines = {idx: search_engines[idx](corpus=corpus[idx], **default_params[idx])
-                              for idx in search_engines}
-
 META = {'Year': 1, 'Program': 2, 'Student': 3, 'Supervisor': 4}
 N_RESULTS = 10
 
@@ -30,10 +27,11 @@ def results():
     if request.method == 'POST':
         idx_type = request.form['index']
         query = request.form['query']
+        search_engine = search_engines[idx_type](corpus=corpus[idx_type], **default_params[idx_type])
 
         start = time.time()
-        results = search_theses(query=query, nlp=fast_nlp, search_engine=initialized_search_engines[idx_type],
-                                db=db, n=N_RESULTS)
+        results = search_theses(query=query, nlp=fast_nlp, search_engine=search_engine,
+                                                        db=db, n=N_RESULTS)
         exec_time = str(round(time.time() - start, 4)) + ' s'
 
         return render_template("result.html",
